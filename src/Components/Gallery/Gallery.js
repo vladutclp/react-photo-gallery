650cb75346fa11f9285c71e5f0addef3c061a8f3
vladutclp <vladut.clapou@gmail.com>
2021-12-01 10:51:51 +0200

    Populated Gallery with dummy data from Unsplash API

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -1,23 +1,53 @@
-import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
+import { useState } from 'react/cjs/react.development';
+import clalsses from './Gallery.module.scss';
+
 
 const Gallery = () => {
-  // useEffect(() => {
-  //   fetch('https://api.unsplash.com/photos?page=1')
-  //     .then(res => res.json())
-  //     .then(
-  //       (result) => {
-  //         console.log(result)
-  //       }
-  //     );
-  // }, []);
+  const getPhotoElements =  (photoData, photoSize) => {
+    console.log('from getPhotoElements');
+    console.log(photoData);
+    if (photoData) {
+      return photoData.map((photo) => {
+        return <img key={photo.id} src={photo.smallSize} alt='' />;
+      });
+    }
+    return null;
+  };
+
+  const [photoData, setPhotoData] = useState();
+  useEffect(() => {
+    const fetchPhotos = async () => {
+      const response = await fetch('https://api.unsplash.com/photos?page=1', {
+        headers: {
+          Authorization:
+            'Client-ID 2IJhGC9QLiwhk3GLACKbd3nv2jWjEUXL_BQYHgW4a58',
+        },
+      });
+      const photoData = await response.json();
+      console.log(photoData)
+      const transformedData = photoData.map((photo) => {
+        return {
+          id: photo.id,
+          regularSize: photo.urls.regular,
+          smallSize: photo.urls.small,
+          thumbSize: photo.urls.thumb,
+        };
+      });
+      console.log('transformedData')
+      console.log(transformedData)
+      if (transformedData) {
+        setPhotoData(transformedData);
+      }
+    };
+
+    fetchPhotos();
+  
+  }, []);
 
   return (
-    <div>
-      <main>Welcome to gallery!</main>
-      <nav>
-        <Link to='/'>Home</Link>
-      </nav>
+    <div className={clalsses.gallery}>
+      <section>{getPhotoElements(photoData)}</section>
     </div>
   );
 };
