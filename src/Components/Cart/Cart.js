9133b894b56434752b06d4982b24db3fdcdd29fa
Vladut Clapou <vladut.clapou@lgepartner.com>
2021-12-23 21:30:28 +0200

    Created Cart and CartItem components; connected them to Redux store

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -3,23 +3,43 @@ import { connect } from 'react-redux';
 import styles from './Cart.module.scss';
 import CartItem from './CartItem';
 const Cart = ({ cartItems }) => {
-  // const getTotalCartItems = (cartItems) => {
-  //   let totalCartItems = 0;
-  //   console.log('from cartItems function');
-  //   console.log(cartItems);
-  //   cartItems.forEach((cartItem) => {
-  //     console.log(cartItem);
-  //     totalCartItems += cartItem.qty;
-  //   });
-  //   console.log(totalCartItems)
-  //   return totalCartItems;
-  // };
-  // console.log('total cart items: ', getTotalCartItems(cartItems));
-  // return <div className={styles.cart}>{getTotalCartItems(cartItems)}</div>;
-  const cartItemElements = cartItems.map((cartItem) => (
-    <CartItem cartItem={cartItem} />
-  ));
-  return <div>{cartItemElements}</div>;
+  console.log(cartItems);
+  const getTotalCartPrice = (cartItems) => {
+    let totalPrice = 0;
+    cartItems.forEach(
+      (cartItem) => (totalPrice += cartItem.qty * cartItem.price)
+    );
+    return totalPrice;
+  };
+  const getDisplayContent = (cartItems) => {
+    if (cartItems.length === 0) {
+      return (
+        <p className={styles.cart__empty}>
+          You have nothing in your shopping cart. Continue Shopping
+        </p>
+      );
+    }
+
+    return cartItems.map((cartItem) => (
+      <CartItem key={cartItem.id} cartItem={cartItem} />
+    ));
+  };
+  return (
+    <div className={styles.cart}>
+      <div className={styles.cartWrapper}>
+        <h1 className={styles.cart__heading}>Shopping cart</h1>
+        {getDisplayContent(cartItems)}
+        {cartItems.length > 0 && (
+          <h2 className={styles.cart__totalPrice}>
+            Subtotal
+            <span className={styles.cart__totalPriceValue}>
+              {getTotalCartPrice(cartItems)}$
+            </span>
+          </h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
--- src/Components/Cart/CartItem.js
+++ src/Components/Cart/CartItem.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import styles from './CartItem.module.scss';
+import { connect } from 'react-redux';
+import {
+  adjustQty,
+  removeFromCart,
+} from '../../store/Shopping/shopping-actions';
+import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
-const CartItem = ({cartItem}) => {
-  return <div>{cartItem.label} x {cartItem.qty}</div>;
+const CartItem = ({ removeFromCart, adjustQty, cartItem }) => {
+  useEffect(() => {
+    if (cartItem.qty < 1) {
+      removeFromCart(cartItem.id);
+      return null;
+    }
+  }, [cartItem]);
+  return (
+    <div className={styles.cartItem}>
+      <div className={styles.cartItem__ImageWrapper}>
+        <img
+          className={styles.cartItem__Image}
+          src={cartItem.url}
+          alt="photo"
+        />
+        <p className={styles.cartItem__Label}>{cartItem.label}</p>
+      </div>
+      <div>
+        <input
+          onChange={(e) => {
+            console.log('from onChangeHandler');
+            console.log(e.target.value);
+            adjustQty(cartItem.id, e.target.value);
+          }}
+          value={cartItem.qty}
+          className={styles.cartItem__QtyInput}
+          type="text"
+        />
+        <button onClick={() => removeFromCart(cartItem.id)} className={styles.cartItem__DeleteButton}>
+          <FontAwesomeIcon icon={faTimes} />
+        </button>
+      </div>
+      <p className={styles.cartItem__Price}>{cartItem.price * cartItem.qty}$</p>
+    </div>
+  );
 };
 
-export default CartItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    adjustQty: (qty, id) => dispatch(adjustQty(qty, id)),
+    removeFromCart: (id) => dispatch(removeFromCart(id)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(CartItem);
