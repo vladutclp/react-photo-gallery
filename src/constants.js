7b68a12795383b4b0e85c6fd4d84e32d33ca3b39
vladutclp <vladut.clapou@gmail.com>
2021-12-31 13:30:47 +0200

    Navbar style changed to fit the rest of the app

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BORDER_COLOR = '#F1B24A';
+export const BORDER_COLOR = 'white';
 export const FONT_COLOR = 'gray';
 export const IMAGES = [
   'https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
