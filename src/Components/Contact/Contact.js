7c51d03cc939b1c4aa51e95cde797d3efbf295b0
vladutclp <vladut.clapou@gmail.com>
2022-02-17 09:56:02 +0200

    Improved contact page

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,42 +1,115 @@
+import Button from '../UI/Button/Button';
 import classes from './Contact.module.scss';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {useNavigate} from 'react-router-dom';
 
 const Contact = () => {
+
+  let navigate = useNavigate();
+  const redirectToHome = () => {
+    let path = '/'
+    navigate(path)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('message was sent')
+    notify();
+    redirectToHome();
+  }
+
+  const notify = () =>
+  toast.success('Message sent!', {
+    position: 'top-center',
+    autoClose: 500,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+  });
+
   return (
     <div className={classes.contact}>
-      <div className={classes.contact__infoWrapper}>
-        <div className={classes.contact__infoHeading}>Get in touch</div>
-        <div className={classes.contact__infoDescription}>
+      <ToastContainer />
+      <div className={classes.description}>
+        <div className={classes.description__heading}>Get in touch</div>
+        <div className={classes.description__text}>
           If you have any questions regarding my gear, workflow or prints don't
           heistate to contact me. You can reach me via email with or on my
           social media accounts.
         </div>
       </div>
-      <form action='' className={classes.contact__form}>
-        <div className={classes.contact__nameFields}>
-          <div className={classes.contact__inputWrapper}>
-            <label htmlFor='name'>Name</label>
-            <input type='text' name='name' id='' />
+      <form action='' className={classes.form} autoComplete='off'>
+        <div className={classes.form__nameFields}>
+          <div className={classes.form__group}>
+            <input
+              className={classes.form__input}
+              type='text'
+              name='name'
+              placeholder='Name'
+              required
+            />
+            <label className={classes.form__label} htmlFor='name'>
+              Name
+            </label>
           </div>
-          <div className={classes.contact__inputWrapper}>
-            <label htmlFor='lastName'>Last Name</label>
-            <input type='text' name='lastName' id='' />
+          <div className={classes.form__group}>
+            <input
+              className={classes.form__input}
+              type='text'
+              name='lastName'
+              placeholder='Last name'
+              required
+            />
+            <label className={classes.form__label} htmlFor='lastName'>
+              Last Name
+            </label>
           </div>
         </div>
-        <div className={classes.contact__inputWrapper}>
-          <label htmlFor='email'>Email</label>
-          <input type='email' name='email' />
+        <div className={classes.form__group}>
+          <input
+            className={classes.form__input}
+            type='email'
+            name='email'
+            placeholder='Email'
+            required
+          />
+          <label className={classes.form__label} htmlFor='email'>
+            Email
+          </label>
         </div>
-        <div className={classes.contact__inputWrapper}>
-          <label htmlFor='subject'>Subject</label>
-          <input type='text' name='subject' />
+        <div className={classes.form__group}>
+          <input
+            className={classes.form__input}
+            type='text'
+            name='subject'
+            placeholder='Subject'
+            required
+          />
+          <label className={classes.form__label} htmlFor='subject'>
+            Subject
+          </label>
         </div>
 
-        <div className={classes.contact__inputWrapper}>
-          <label htmlFor='message'>Message</label>
-          <textarea name='Message' id='' cols='30' rows='5' />
+        <div className={classes.form__group}>
+          <textarea
+            className={classes.form__input}
+            name='Message'
+            cols='30'
+            rows='3'
+            placeholder='Message'
+            required
+          />
+          <label className={classes.form__label} htmlFor='message'>
+            Message
+          </label>
         </div>
 
-        <button className={classes.contact__submit}>Submit</button>
+        <Button clickHandler={submitHandler} className={classes.form__submit} label={'Send message'}>
+          Submit
+        </Button>
       </form>
     </div>
   );
--- src/Components/UI/Button/Button.js
+++ src/Components/UI/Button/Button.js
@@ -4,7 +4,7 @@ import classes from './Button.module.scss';
 
 const Button = (props) => {
   return (
-    <button className={classes.button} onClick={props.clickHandler}>
+    <button className={`${classes.button} ${props.className}`} onClick={props.clickHandler}>
       <span> {props.icon && <FontAwesomeIcon icon={props.icon} />}</span>
       <span className={classes.button__label}>{props.label}</span>
     </button>
